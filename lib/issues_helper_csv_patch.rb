d77c043da1003040aa39d31fb52aca70f82d948d
MAEDA Go <maeda@farend.jp>
2012-10-02 17:14:44 +0900

    Redmine 2.1 compatibility.

--- lib/issues_helper_csv_patch.rb
+++ lib/issues_helper_csv_patch.rb
@@ -13,33 +13,33 @@ module IssuesHelperCsvPatch
       decimal_separator = l(:general_csv_decimal_separator)
       encoding = l(:general_csv_encoding)
       columns = (options[:columns] == 'all' ? query.available_columns : query.columns)
-  
+
       export = FCSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
         # csv header fields
         csv << [ "#" ] + columns.collect {|c| Redmine::CodesetUtil.from_utf8(c.caption.to_s, encoding) } +
-          (options[:description] ? [Redmine::CodesetUtil.from_utf8(l(:field_description), encoding), Redmine::CodesetUtil.from_utf8(l(:field_notes), encoding)] : [])
-  
+          (options[:description] ? [Redmine::CodesetUtil.from_utf8(l(:field_description), encoding)] : [])
+
         # csv lines
         issues.each do |issue|
           col_values = columns.collect do |column|
             s = if column.is_a?(QueryCustomFieldColumn)
-              cv = issue.custom_values.detect {|v| v.custom_field_id == column.custom_field.id}
+              cv = issue.custom_field_values.detect {|v| v.custom_field_id == column.custom_field.id}
               show_value(cv)
             else
-              value = issue.send(column.name)
+              value = column.value(issue)
               if value.is_a?(Date)
                 format_date(value)
               elsif value.is_a?(Time)
                 format_time(value)
               elsif value.is_a?(Float)
-                value.to_s.gsub('.', decimal_separator)
+                ("%.2f" % value).gsub('.', decimal_separator)
               else
                 value
               end
             end
             s.to_s
           end
-          
+
           latest_journal = issue.journals.reverse.detect {|j| ! j.notes.to_s.empty?}
           if latest_journal
             note_datetime = latest_journal.created_on.strftime("%Y/%m/%d %H:%M")
@@ -49,7 +49,7 @@ module IssuesHelperCsvPatch
           else
             journal_text = ""
           end
-              
+
           csv << [ issue.id.to_s ] + col_values.collect {|c| Redmine::CodesetUtil.from_utf8(c.to_s, encoding) } +
             (options[:description] ? [Redmine::CodesetUtil.from_utf8(issue.description, encoding)] + [Redmine::CodesetUtil.from_utf8(journal_text, encoding)] : [])
         end
