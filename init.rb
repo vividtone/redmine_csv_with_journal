ce0433b0b1e076b23056c0090e1d0de841121980
MAEDA Go <maeda@farend.jp>
2013-05-30 16:10:18 +0900

    Redmine 2.3 compatibility

--- init.rb
+++ init.rb
@@ -1,11 +1,14 @@
 require 'redmine'
-require 'issues_helper_csv_patch'
+
+Rails.configuration.to_prepare do
+  require_dependency 'issues_helper_csv_patch'
+end
 
 Redmine::Plugin.register :redmine_csv_with_journal do
   name 'Redmine Csv With Journal plugin'
   author 'MAEDA Go'
   description "CSV output with issue's latest notes."
-  version '0.0.1'
+  version '20130530'
   url ''
   author_url ''
 end
--- lib/issues_helper_csv_patch.rb
+++ lib/issues_helper_csv_patch.rb
@@ -1,62 +1,46 @@
-require_dependency 'issues_helper'
+require_dependency 'queries_helper'
 
-module IssuesHelperCsvPatch
+module QueriesHelperCsvPatch
   def self.included(base)
     base.module_eval do
       include OverrideMethods
-      alias_method_chain :issues_to_csv, :journal
+      alias_method_chain :query_to_csv, :journal
     end
   end
 
   module OverrideMethods
-    def issues_to_csv_with_journal(issues, project, query, options={})
-      decimal_separator = l(:general_csv_decimal_separator)
+    def query_to_csv_with_journal(items, query, options={})
       encoding = l(:general_csv_encoding)
-      columns = (options[:columns] == 'all' ? query.available_columns : query.columns)
+      columns = (options[:columns] == 'all' ? query.available_inline_columns : query.inline_columns)
+      query.available_block_columns.each do |column|
+        if options[column.name].present?
+          columns << column
+        end
+      end
 
       export = FCSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
         # csv header fields
-        csv << [ "#" ] + columns.collect {|c| Redmine::CodesetUtil.from_utf8(c.caption.to_s, encoding) } +
-          (options[:description] ? [Redmine::CodesetUtil.from_utf8(l(:field_description), encoding)] : [])
-
+        csv << columns.collect {|c| Redmine::CodesetUtil.from_utf8(c.caption.to_s, encoding) }
         # csv lines
-        issues.each do |issue|
-          col_values = columns.collect do |column|
-            s = if column.is_a?(QueryCustomFieldColumn)
-              cv = issue.custom_field_values.detect {|v| v.custom_field_id == column.custom_field.id}
-              show_value(cv)
-            else
-              value = column.value(issue)
-              if value.is_a?(Date)
-                format_date(value)
-              elsif value.is_a?(Time)
-                format_time(value)
-              elsif value.is_a?(Float)
-                ("%.2f" % value).gsub('.', decimal_separator)
-              else
-                value
-              end
+        items.each do |item|
+          # begin patch
+          colvalues = columns.collect {|c| Redmine::CodesetUtil.from_utf8(csv_content(c, item), encoding) }
+          if options[:description] && item.is_a?(Issue)
+            latest_journal = item.journals.reverse.detect {|j| ! j.notes.to_s.empty?}
+            if latest_journal
+              note_datetime = latest_journal.created_on.strftime("%Y/%m/%d %H:%M")
+              note_user = latest_journal.user.lastname + " " + latest_journal.user.firstname
+              note = latest_journal.notes
+              colvalues << Redmine::CodesetUtil.from_utf8("(#{note_datetime} #{note_user})\r\n#{note}!!!", encoding)
             end
-            s.to_s
           end
-
-          latest_journal = issue.journals.reverse.detect {|j| ! j.notes.to_s.empty?}
-          if latest_journal
-            note_datetime = latest_journal.created_on.strftime("%Y/%m/%d %H:%M")
-            note_user = latest_journal.user.lastname + " " + latest_journal.user.firstname
-            note = latest_journal.notes
-            journal_text = "(#{note_datetime} #{note_user})\r\n#{note}"
-          else
-            journal_text = ""
-          end
-
-          csv << [ issue.id.to_s ] + col_values.collect {|c| Redmine::CodesetUtil.from_utf8(c.to_s, encoding) } +
-            (options[:description] ? [Redmine::CodesetUtil.from_utf8(issue.description, encoding)] + [Redmine::CodesetUtil.from_utf8(journal_text, encoding)] : [])
+          csv << colvalues
+          # end patch
         end
       end
-      export
+      export  
     end
   end
 end
 
-IssuesHelper.send(:include, IssuesHelperCsvPatch)
+QueriesHelper.send(:include, QueriesHelperCsvPatch)
